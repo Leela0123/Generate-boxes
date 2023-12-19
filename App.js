3c233862ebca64f408448edcc8f4e576aa91f51b
Leela Kumari <117524740+Leela0123@users.noreply.github.com>
2023-12-19 15:10:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,25 @@
 
-    function generateBoxes() {
+    // function generateBoxes() {
+    //   const boxNumber = document.getElementById('boxNumber').value;
+    //   const boxContainer = document.getElementById('boxContainer');
+
+    //   // Clear existing boxes
+    //   boxContainer.innerHTML = '';
+
+    //   // Calculate box size dynamically based on the container size and the number of boxes
+    //   const boxSize = Math.min(boxContainer.clientWidth / boxNumber, boxContainer.clientHeight / boxNumber);
+
+    //   // Generate new boxes within the container
+    //   for (let i = 0; i < boxNumber; i++) {
+    //     const box = document.createElement('div');
+    //     box.className = 'box';
+    //     box.style.width = box.style.height = boxSize + 'px';
+    //     box.style.bottom = i * boxSize + 'px'; // Adjust spacing as needed
+    //     box.style.left = i * boxSize + 'px';   // Adjust spacing as needed
+    //     boxContainer.appendChild(box);
+    //   }
+    // }
+   function generateBoxes() {
       const boxNumber = document.getElementById('boxNumber').value;
       const boxContainer = document.getElementById('boxContainer');
 
@@ -7,16 +27,17 @@
       boxContainer.innerHTML = '';
 
       // Calculate box size dynamically based on the container size and the number of boxes
-      const boxSize = Math.min(boxContainer.clientWidth / boxNumber, boxContainer.clientHeight / boxNumber);
+      const boxHeight = boxContainer.clientHeight / boxNumber;
+      const boxWidth = boxContainer.clientWidth/boxNumber; // Set the width to be twice the height
 
       // Generate new boxes within the container
       for (let i = 0; i < boxNumber; i++) {
         const box = document.createElement('div');
         box.className = 'box';
-        box.style.width = box.style.height = boxSize + 'px';
-        box.style.bottom = i * boxSize + 'px'; // Adjust spacing as needed
-        box.style.left = i * boxSize + 'px';   // Adjust spacing as needed
+        box.style.width = boxWidth + 'px';
+        box.style.height = boxHeight + 'px';
+        box.style.bottom = i * boxHeight + 'px'; // Adjust spacing as needed
+        box.style.left = i * boxWidth + 'px';   // Adjust spacing as needed
         boxContainer.appendChild(box);
       }
     }
-  
